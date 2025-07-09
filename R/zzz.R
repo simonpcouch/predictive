96a04af231fd3fffc16ee67223ea197dbdb4fbe4
simonpcouch <simonpatrickcouch@gmail.com>
2025-07-09 16:29:14 -0500

    load reasonably assumed pkgs and data

--- R/zzz.R
+++ R/zzz.R
@@ -5,5 +5,12 @@
   ) {
     load("inst/sandbox/starter_turns.rda")
     globals$turns <- starter_turns
+    library(tidymodels)
+    library(modeldata)
+    assign(
+      "so_folds",
+      vfold_cv(stackoverflow, strata = Salary),
+      envir = .GlobalEnv
+    )
   }
 }
